a5f821c616fa112c76fbf30615b6fe4bb825ce3e
zengtao <zt@xlbsoft.com>
2025-04-27 08:36:24 +0800

    refactor(sql): 更新数据库结构和数据
    - 修改了 permission、role、user 等表的结构
    - 更新了相关表的记录数据
    - 调整了字段顺序和数据类型
    -优化了索引设置

--- sql/nest.sql
+++ sql/nest.sql
@@ -3,15 +3,15 @@
 
  Source Server         : 本地
  Source Server Type    : MySQL
- Source Server Version : 80040 (8.0.40)
- Source Host           : 192.168.31.78:3306
+ Source Server Version : 80037
+ Source Host           : localhost:3306
  Source Schema         : nest
 
  Target Server Type    : MySQL
- Target Server Version : 80040 (8.0.40)
+ Target Server Version : 80037
  File Encoding         : 65001
 
- Date: 19/02/2025 21:24:39
+ Date: 27/04/2025 08:35:08
 */
 
 SET NAMES utf8mb4;
@@ -29,8 +29,8 @@ CREATE TABLE `permission`  (
   `updated_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `sign` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `parentId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `sort` int NOT NULL,
+  `parentId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `FK_41c446d29bbb76111ace88bcc59`(`parentId` ASC) USING BTREE,
   CONSTRAINT `FK_41c446d29bbb76111ace88bcc59` FOREIGN KEY (`parentId`) REFERENCES `permission` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
@@ -39,10 +39,10 @@ CREATE TABLE `permission`  (
 -- ----------------------------
 -- Records of permission
 -- ----------------------------
-INSERT INTO `permission` VALUES ('14b98651-87c7-46e4-882b-854f973516f7', '2025-02-19 13:11:47', NULL, '2025-02-19 13:11:47', NULL, '用户管理', 'settingAuthManagerUser', 'ff167114-a97b-4b15-92aa-b348d8f10a7d', 1);
-INSERT INTO `permission` VALUES ('90e1bb81-95fe-4e77-ae60-87ab73ea455d', '2025-02-19 13:11:53', NULL, '2025-02-19 13:11:53', NULL, '角色管理', 'settingAuthManagerRole', 'ff167114-a97b-4b15-92aa-b348d8f10a7d', 2);
-INSERT INTO `permission` VALUES ('fa2ed1ac-89c2-4e11-8de2-7cb5f53dc03e', '2025-02-19 13:11:59', NULL, '2025-02-19 13:11:59', NULL, '权限管理', 'settingAuthManagerPermission', 'ff167114-a97b-4b15-92aa-b348d8f10a7d', 3);
-INSERT INTO `permission` VALUES ('ff167114-a97b-4b15-92aa-b348d8f10a7d', '2025-02-19 13:11:40', NULL, '2025-02-19 13:11:40', NULL, '设置', 'setting', NULL, 1);
+INSERT INTO `permission` VALUES ('81dc89f6-c3aa-4e32-86bc-a7fef952feca', '2025-01-24 17:09:37', NULL, '2025-01-24 17:09:37', NULL, '用户管理', 'settingAuthManagerUser', 1, 'b2f8f794-78d0-41b9-a4ce-9f7c7f808d66');
+INSERT INTO `permission` VALUES ('b2f8f794-78d0-41b9-a4ce-9f7c7f808d66', '2025-01-24 16:17:27', NULL, '2025-01-24 16:17:27', NULL, '设置', 'setting', 1, NULL);
+INSERT INTO `permission` VALUES ('c57d427e-15be-4a0e-9e57-4303b99dcb05', '2025-01-24 17:09:50', NULL, '2025-01-24 17:26:10', NULL, '角色管理', 'settingAuthManagerRole', 1, 'b2f8f794-78d0-41b9-a4ce-9f7c7f808d66');
+INSERT INTO `permission` VALUES ('fa256a8a-5477-4353-a621-66662184bfbc', '2025-01-24 17:09:46', NULL, '2025-01-24 17:46:11', NULL, '权限管理', 'settingAuthManagerPermission', 3, 'b2f8f794-78d0-41b9-a4ce-9f7c7f808d66');
 
 -- ----------------------------
 -- Table structure for permission_closure
@@ -61,13 +61,13 @@ CREATE TABLE `permission_closure`  (
 -- ----------------------------
 -- Records of permission_closure
 -- ----------------------------
-INSERT INTO `permission_closure` VALUES ('14b98651-87c7-46e4-882b-854f973516f7', '14b98651-87c7-46e4-882b-854f973516f7');
-INSERT INTO `permission_closure` VALUES ('90e1bb81-95fe-4e77-ae60-87ab73ea455d', '90e1bb81-95fe-4e77-ae60-87ab73ea455d');
-INSERT INTO `permission_closure` VALUES ('fa2ed1ac-89c2-4e11-8de2-7cb5f53dc03e', 'fa2ed1ac-89c2-4e11-8de2-7cb5f53dc03e');
-INSERT INTO `permission_closure` VALUES ('ff167114-a97b-4b15-92aa-b348d8f10a7d', '14b98651-87c7-46e4-882b-854f973516f7');
-INSERT INTO `permission_closure` VALUES ('ff167114-a97b-4b15-92aa-b348d8f10a7d', '90e1bb81-95fe-4e77-ae60-87ab73ea455d');
-INSERT INTO `permission_closure` VALUES ('ff167114-a97b-4b15-92aa-b348d8f10a7d', 'fa2ed1ac-89c2-4e11-8de2-7cb5f53dc03e');
-INSERT INTO `permission_closure` VALUES ('ff167114-a97b-4b15-92aa-b348d8f10a7d', 'ff167114-a97b-4b15-92aa-b348d8f10a7d');
+INSERT INTO `permission_closure` VALUES ('81dc89f6-c3aa-4e32-86bc-a7fef952feca', '81dc89f6-c3aa-4e32-86bc-a7fef952feca');
+INSERT INTO `permission_closure` VALUES ('b2f8f794-78d0-41b9-a4ce-9f7c7f808d66', '81dc89f6-c3aa-4e32-86bc-a7fef952feca');
+INSERT INTO `permission_closure` VALUES ('b2f8f794-78d0-41b9-a4ce-9f7c7f808d66', 'b2f8f794-78d0-41b9-a4ce-9f7c7f808d66');
+INSERT INTO `permission_closure` VALUES ('b2f8f794-78d0-41b9-a4ce-9f7c7f808d66', 'c57d427e-15be-4a0e-9e57-4303b99dcb05');
+INSERT INTO `permission_closure` VALUES ('b2f8f794-78d0-41b9-a4ce-9f7c7f808d66', 'fa256a8a-5477-4353-a621-66662184bfbc');
+INSERT INTO `permission_closure` VALUES ('c57d427e-15be-4a0e-9e57-4303b99dcb05', 'c57d427e-15be-4a0e-9e57-4303b99dcb05');
+INSERT INTO `permission_closure` VALUES ('fa256a8a-5477-4353-a621-66662184bfbc', 'fa256a8a-5477-4353-a621-66662184bfbc');
 
 -- ----------------------------
 -- Table structure for role
@@ -88,7 +88,8 @@ CREATE TABLE `role`  (
 -- ----------------------------
 -- Records of role
 -- ----------------------------
-INSERT INTO `role` VALUES ('c01b6584-b999-487c-ad57-54cd2219b0f5', '2025-02-19 13:16:56', NULL, '2025-02-19 13:16:56', NULL, '管理员', '管理员', '1');
+INSERT INTO `role` VALUES ('86bbe707-c834-446a-998f-6ce11ec08ab5', '2025-01-08 17:58:13', NULL, '2025-01-21 16:24:34', NULL, '管理员', '别乱改了', '');
+INSERT INTO `role` VALUES ('c4672de0-87bc-4185-a270-84598850abbc', '2025-01-08 17:39:59', NULL, '2025-02-05 13:59:00', NULL, '小明的', '试试', '');
 
 -- ----------------------------
 -- Table structure for role_permissions_permission
@@ -107,15 +108,17 @@ CREATE TABLE `role_permissions_permission`  (
 -- ----------------------------
 -- Records of role_permissions_permission
 -- ----------------------------
-INSERT INTO `role_permissions_permission` VALUES ('c01b6584-b999-487c-ad57-54cd2219b0f5', 'ff167114-a97b-4b15-92aa-b348d8f10a7d');
+INSERT INTO `role_permissions_permission` VALUES ('86bbe707-c834-446a-998f-6ce11ec08ab5', 'b2f8f794-78d0-41b9-a4ce-9f7c7f808d66');
+INSERT INTO `role_permissions_permission` VALUES ('c4672de0-87bc-4185-a270-84598850abbc', '81dc89f6-c3aa-4e32-86bc-a7fef952feca');
+INSERT INTO `role_permissions_permission` VALUES ('c4672de0-87bc-4185-a270-84598850abbc', 'fa256a8a-5477-4353-a621-66662184bfbc');
 
 -- ----------------------------
 -- Table structure for role_users_user
 -- ----------------------------
 DROP TABLE IF EXISTS `role_users_user`;
 CREATE TABLE `role_users_user`  (
-  `roleId` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `userId` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `roleId` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`roleId`, `userId`) USING BTREE,
   INDEX `IDX_ed6edac7184b013d4bd58d60e5`(`roleId` ASC) USING BTREE,
   INDEX `IDX_a88fcb405b56bf2e2646e9d479`(`userId` ASC) USING BTREE,
@@ -137,20 +140,21 @@ CREATE TABLE `user`  (
   `created_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '1',
-  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `tel` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('121212', '2025-02-16 16:39:40', NULL, '2025-02-19 13:17:12', NULL, '小明', '1', NULL, 'admin', 'U2FsdGVkX1/gvVCSzc5zo8MAfMpouJ4e//r+jJkjGhN2ZufuSsssDy1JowwdFIDviUYLogbs9F2t3xn+HdkfTg==', NULL, NULL);
+INSERT INTO `user` VALUES ('550c0485-5e54-4fbf-8ae4-5ac60ef383df', '2025-01-07 17:28:09', NULL, '2025-02-05 15:20:23', NULL, '17761744292', 'U2FsdGVkX18CNyzk9H7wvLnoFT0TLGLOQoyyySthwgo=', NULL, '448987786@qq.com', '小明', NULL, '1');
+INSERT INTO `user` VALUES ('ec1581f9-6fc8-4aee-ab41-b4f4318d4cd3', '2025-01-21 14:20:05', NULL, '2025-01-23 16:04:30', NULL, 'wwz', 'U2FsdGVkX1/Ml9c438kJQDaIjdngJJa0lnmZRUv4yAE=', NULL, NULL, '小真', NULL, '1');
 
 -- ----------------------------
 -- Table structure for user_roles_role
@@ -169,6 +173,7 @@ CREATE TABLE `user_roles_role`  (
 -- ----------------------------
 -- Records of user_roles_role
 -- ----------------------------
-INSERT INTO `user_roles_role` VALUES ('121212', 'c01b6584-b999-487c-ad57-54cd2219b0f5');
+INSERT INTO `user_roles_role` VALUES ('550c0485-5e54-4fbf-8ae4-5ac60ef383df', '86bbe707-c834-446a-998f-6ce11ec08ab5');
+INSERT INTO `user_roles_role` VALUES ('ec1581f9-6fc8-4aee-ab41-b4f4318d4cd3', 'c4672de0-87bc-4185-a270-84598850abbc');
 
 SET FOREIGN_KEY_CHECKS = 1;
